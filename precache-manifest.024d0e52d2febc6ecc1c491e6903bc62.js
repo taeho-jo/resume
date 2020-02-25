d5c9a9a3d021a43d580d8be0592285e633cb981b
jotang <jotang3726@gmail.com>
2020-02-25 21:58:35 +0900

    Updates

---
+++ precache-manifest.024d0e52d2febc6ecc1c491e6903bc62.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5533b1666fe87773657d74315d4503f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "82a0e6e69f2892a11b55",
+    "url": "/resume/static/js/2.796f25aa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/resume/static/js/2.796f25aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eef5cd6be5a452255602",
+    "url": "/resume/static/js/main.cdb162eb.chunk.js"
+  },
+  {
+    "revision": "fc6ccfdad72e7b5d0278",
+    "url": "/resume/static/js/runtime-main.4beab0ad.js"
+  },
+  {
+    "revision": "52c3a1cf674133b3ec2c4c316180ae14",
+    "url": "/resume/static/media/KeepCalm-Medium.52c3a1cf.ttf"
+  },
+  {
+    "revision": "21c892a7f38e340a2c73f61f1bb86c34",
+    "url": "/resume/static/media/Rounded_Elegance.21c892a7.ttf"
+  },
+  {
+    "revision": "4420630ccc21b61751219a28960be7b0",
+    "url": "/resume/static/media/dream_orphans.4420630c.ttf"
+  },
+  {
+    "revision": "ebc87fb3783fea31cd046ff521890f83",
+    "url": "/resume/static/media/justsell.ebc87fb3.png"
+  },
+  {
+    "revision": "583744dad31036d50e3d9ff394e4acc8",
+    "url": "/resume/static/media/profile2.583744da.jpg"
+  }
+]);
